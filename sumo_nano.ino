4f444e1f54bd52c5f8e9f628ab317be7d4cdb42e
Phuong Huy <phuonghuyhvhk@gmail.com>
2023-03-15 23:45:06 +0700

    Done Line Sensor

--- sumo_nano.ino
+++ sumo_nano.ino
@@ -1,52 +1,19 @@
 // Khai báo các chân kết nối giữa L298N và Arduino Nano
-const int enA = 9;
-const int in1 = 8;
-const int in2 = 7;
 
-const int enB = 12;
-const int in3 = 10;
-const int in4 = 11;
+int value1, value2;
 
 void setup() {
-  // Các chân được sử dụng là đầu ra
-  pinMode(enA, OUTPUT);
-  pinMode(in1, OUTPUT);
-  pinMode(in2, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
 
-  // Bật đầu ra PWM trên chân enA
-  analogWrite(enA, 256);
-  analogWrite(enB, 256);
+    Serial.begin(9600);
+    pinMode(3, INPUT);
+    pinMode(4, INPUT);
 }
 
 void loop() {
-  // Quay về phía trước
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  delay(2000);
-
-  // Dừng lại
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  delay(1000);
-
-  // Quay về phía sau
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  delay(2000);
-
-  // Dừng lại
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  delay(1000);
+    value1 = digitalRead(3);
+    Serial.println(value1);
+    Serial.println("**********");
+    value2 = digitalRead(4);
+    Serial.println(value2);
+    delay(1000);
 }
