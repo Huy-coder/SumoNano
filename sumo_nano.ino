81590a20c532dafb809766fcbe0a3ffd7d84ccba
Phuong Huy <phuonghuyhvhk@gmail.com>
2023-03-16 13:41:28 +0700

    Test DC

--- sumo_nano.ino
+++ sumo_nano.ino
@@ -15,6 +15,15 @@
 //Model IR Sensor
 #define MODEL 1080
 
+//Motor DC
+#define EN_A 6
+#define IN_1 7
+#define IN_2 8
+
+#define EN_B 11
+#define IN_3 9
+#define IN_4 10
+
 SharpIR Backward_IR = SharpIR(BACKWARD_SENSOR, MODEL);
 SharpIR Left_IR = SharpIR(LEFT_SENSOR, MODEL);
 SharpIR Right_IR = SharpIR(RIGHT_SENSOR, MODEL);
@@ -26,6 +35,12 @@ LineSensor lineSensor;
 void setup() {
 
     Serial.begin(9600);
+    pinMode(EN_A, OUTPUT);
+    pinMode(IN_1, OUTPUT);
+    pinMode(IN_2, OUTPUT);
+    pinMode(EN_B, OUTPUT);
+    pinMode(IN_3, OUTPUT);
+    pinMode(IN_4, OUTPUT);
     
     pinMode(FRONT_SENSOR, INPUT);
     pinMode(BACK_SENSOR, INPUT);
@@ -43,5 +58,20 @@ void loop() {
     Serial.println(digitalRead(FRONT_SENSOR));
     Serial.print("Cam bien do line phia sau: ");
     Serial.println(digitalRead(BACK_SENSOR));
-    delay(1000);
+
+    analogWrite(EN_A, 256);
+    analogWrite(EN_B, 256);
+    digitalWrite(IN_1, HIGH);
+    digitalWrite(IN_2, LOW);
+    digitalWrite(IN_3, HIGH);
+    digitalWrite(IN_4, LOW);
+    delay(2000);
+
+    analogWrite(EN_A, 100);
+    analogWrite(EN_B, 100);
+    digitalWrite(IN_1, LOW);
+    digitalWrite(IN_2, HIGH);
+    digitalWrite(IN_3, LOW);
+    digitalWrite(IN_4, HIGH);
+    delay(2000);
 }
