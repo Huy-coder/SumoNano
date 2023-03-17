ff31ee90f7825c5ff7cee5fd6d76758acb4e2166
Phuong Huy <phuonghuyhvhk@gmail.com>
2023-03-17 20:59:26 +0700

    Test san

--- sumo_nano.ino
+++ sumo_nano.ino
@@ -1,8 +1,9 @@
 #include <SharpIR.h>
 
 //Read digital value
-#define FRONT_SENSOR 4
-#define BACK_SENSOR 3
+#define LEFT_DETECT_LINE_SENSOR 5
+#define RIGHT_DETECT_LINE_SENSOR 4
+#define BACK_DETECT_LINE_SENSOR 3
 
 //Read analog value
 #define BACKWARD_SENSOR A3
@@ -32,7 +33,6 @@ SharpIR Right_IR = SharpIR(RIGHT_SENSOR, MODEL);
 
 void setup() 
 {
-
     Serial.begin(9600);
     pinMode(EN_A, OUTPUT);
     pinMode(IN_1, OUTPUT);
@@ -40,39 +40,72 @@ void setup()
     pinMode(EN_B, OUTPUT);
     pinMode(IN_3, OUTPUT);
     pinMode(IN_4, OUTPUT);
+    pinMode(LEFT_DETECT_LINE_SENSOR, INPUT);
+    pinMode(RIGHT_DETECT_LINE_SENSOR, INPUT);       
+    pinMode(BACK_DETECT_LINE_SENSOR, INPUT);    
     
-    pinMode(FRONT_SENSOR, INPUT);
-    pinMode(BACK_SENSOR, INPUT);
+    delay(2000);
 }
 
 void loop() 
 {
-    Serial.print("Cam bien sau lung: ");
-    Serial.println(Backward_IR.distance());
-    Serial.print("Cam bien ben trai: ");
-    Serial.println(Left_IR.distance());
-    Serial.print("Cam bien ben phai: ");
-    Serial.println(Right_IR.distance());
-    if(digitalRead(FRONT_SENSOR) == 1)
-        runBackward();
-    else if(digitalRead(BACK_SENSOR) == 1)
-        runForward();
-    else
-        stopMotor();
-    delay(1000);
+    // Serial.print("Cam bien sau lung: ");
+    // Serial.println(Backward_IR.distance());
+    // Serial.print("Cam bien ben trai: ");
+    // Serial.println(Left_IR.distance());
+    // Serial.print("Cam bien ben phai: ");
+    // Serial.println(Right_IR.distance());
+    
+    // while(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 0 && digitalRead(LEFT_DETECT_LINE_SENSOR) == 0 && digitalRead(BACK_DETECT_LINE_SENSOR) == 0)
+    // {
+    //     //findEnemy();
+    // }
+    avoidToDropOut();
+    // delay(1500);
 }
 
 void runForward()
 {
-    analogWrite(EN_A, 255);
-    analogWrite(EN_B, 255);
+    
+    analogWrite(EN_A, 80);
+    analogWrite(EN_B, 80);
+    digitalWrite(IN_1, 1);
+    digitalWrite(IN_2, 0);
+    digitalWrite(IN_3, 0);
+    digitalWrite(IN_4, 1);
+}
+
+void runBackward()
+{
+    analogWrite(EN_A, 80);
+    analogWrite(EN_B, 80);
     digitalWrite(IN_1, 0);
     digitalWrite(IN_2, 1);
     digitalWrite(IN_3, 1);
     digitalWrite(IN_4, 0);
 }
 
-void runBackward()
+void runLeft()
+{
+    analogWrite(EN_A, 255);
+    analogWrite(EN_B, 150);
+    digitalWrite(IN_1, 1);
+    digitalWrite(IN_2, 0);
+    digitalWrite(IN_3, 1);
+    digitalWrite(IN_4, 0);
+}
+
+void runRight()
+{
+    analogWrite(EN_A, 150);
+    analogWrite(EN_B, 255);
+    digitalWrite(IN_1, 0);
+    digitalWrite(IN_2, 1);
+    digitalWrite(IN_3, 0);
+    digitalWrite(IN_4, 1);
+}
+
+void runBackLeft()
 {
     analogWrite(EN_A, 255);
     analogWrite(EN_B, 255);
@@ -82,24 +115,24 @@ void runBackward()
     digitalWrite(IN_4, 1);
 }
 
-void runLeft()
-{
-
-}
-
-void runRight()
-{
-
-}
-
-void runBackLeft()
-{
-  
-}
-
 void runBackRight()
 {
+    analogWrite(EN_A, 255);
+    analogWrite(EN_B, 255);
+    digitalWrite(IN_1, 1);
+    digitalWrite(IN_2, 0);
+    digitalWrite(IN_3, 0);
+    digitalWrite(IN_4, 1);
+}
 
+void round360()
+{
+    analogWrite(EN_A, 150);
+    analogWrite(EN_B, 0);
+    digitalWrite(IN_1, 1);
+    digitalWrite(IN_2, 0);
+    digitalWrite(IN_3, 0);
+    digitalWrite(IN_4, 1);  
 }
 
 void stopMotor()
@@ -112,7 +145,82 @@ void stopMotor()
     digitalWrite(IN_4, 0);
 }
 
+void attackEnemy()
+{
+    analogWrite(EN_A, 255);
+    analogWrite(EN_B, 255);
+    digitalWrite(IN_1, 1);
+    digitalWrite(IN_2, 0);
+    digitalWrite(IN_3, 0);
+    digitalWrite(IN_4, 1);
+}
+
 void findEnemy()
+{
+    if(Left_IR.distance() <= 10 || Right_IR.distance() <= 10)
+    {
+        attackEnemy();
+        delay(500);
+        stopMotor();
+        delay(100);
+        runBackward();
+        delay(500);
+    }
+    else if (Backward_IR.distance() <= 30)
+    {
+        round360();
+        delay(500);
+        attackEnemy();
+        delay(500);
+        stopMotor();
+        delay(100);
+        runBackward();
+        delay(500);        
+    }
+    else
+    {
+        if(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 1 || digitalRead(LEFT_DETECT_LINE_SENSOR) == 1)
+        {
+            runBackward();
+            delay(500);        
+        }
+        else if(digitalRead(BACK_DETECT_LINE_SENSOR) == 1)
+        {
+            runForward();
+            delay(500);
+        }
+        else
+        {
+            runForward();
+            delay(500);            
+            round360();
+            delay(500);
+            runBackward();
+            delay(500);
+            round360();
+            delay(500); 
+        }
+    }
+}
+
+void avoidToDropOut()
+{
+    if(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 1 || digitalRead(LEFT_DETECT_LINE_SENSOR) == 1)
+    {
+        runBackward();
+        delay(500);
+    }
+        
+    else if(digitalRead(BACK_DETECT_LINE_SENSOR) == 1)
+    {
+        runForward();
+        delay(200);
+    }
+    stopMotor();
+    delay(100);
+}
+
+void firstStart()
 {
     
 }
