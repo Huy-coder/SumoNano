9789d6b3850ef64fa4d11f0b0e46e4f3256b54f5
Phuong Huy <phuonghuyhvhk@gmail.com>
2023-03-17 21:13:50 +0700

    Testing

--- sumo_nano.ino
+++ sumo_nano.ino
@@ -31,196 +31,185 @@ SharpIR Backward_IR = SharpIR(BACKWARD_SENSOR, MODEL);
 SharpIR Left_IR = SharpIR(LEFT_SENSOR, MODEL);
 SharpIR Right_IR = SharpIR(RIGHT_SENSOR, MODEL);
 
-void setup() 
-{
-    Serial.begin(9600);
-    pinMode(EN_A, OUTPUT);
-    pinMode(IN_1, OUTPUT);
-    pinMode(IN_2, OUTPUT);
-    pinMode(EN_B, OUTPUT);
-    pinMode(IN_3, OUTPUT);
-    pinMode(IN_4, OUTPUT);
-    pinMode(LEFT_DETECT_LINE_SENSOR, INPUT);
-    pinMode(RIGHT_DETECT_LINE_SENSOR, INPUT);       
-    pinMode(BACK_DETECT_LINE_SENSOR, INPUT);    
-    
-    delay(2000);
+unsigned long time;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(EN_A, OUTPUT);
+  pinMode(IN_1, OUTPUT);
+  pinMode(IN_2, OUTPUT);
+  pinMode(EN_B, OUTPUT);
+  pinMode(IN_3, OUTPUT);
+  pinMode(IN_4, OUTPUT);
+  pinMode(LEFT_DETECT_LINE_SENSOR, INPUT);
+  pinMode(RIGHT_DETECT_LINE_SENSOR, INPUT);
+  pinMode(BACK_DETECT_LINE_SENSOR, INPUT);
+  time = millis();
+
+  delay(2000);
 }
 
-void loop() 
-{
-    // Serial.print("Cam bien sau lung: ");
-    // Serial.println(Backward_IR.distance());
-    // Serial.print("Cam bien ben trai: ");
-    // Serial.println(Left_IR.distance());
-    // Serial.print("Cam bien ben phai: ");
-    // Serial.println(Right_IR.distance());
-    
-    // while(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 0 && digitalRead(LEFT_DETECT_LINE_SENSOR) == 0 && digitalRead(BACK_DETECT_LINE_SENSOR) == 0)
-    // {
-    //     //findEnemy();
-    // }
-    avoidToDropOut();
-    // delay(1500);
+void loop() {
+  // Serial.print("Cam bien sau lung: ");
+  // Serial.println(Backward_IR.distance());
+  // Serial.print("Cam bien ben trai: ");
+  // Serial.println(Left_IR.distance());
+  // Serial.print("Cam bien ben phai: ");
+  // Serial.println(Right_IR.distance());
+
+  // while(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 0 && digitalRead(LEFT_DETECT_LINE_SENSOR) == 0 && digitalRead(BACK_DETECT_LINE_SENSOR) == 0)
+  // {
+  //     //findEnemy();
+  // }
+  avoidToDropOut();
+  // delay(1500);
 }
 
-void runForward()
-{
-    
-    analogWrite(EN_A, 80);
-    analogWrite(EN_B, 80);
-    digitalWrite(IN_1, 1);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 1);
+void runForward() {
+
+  analogWrite(EN_A, 80);
+  analogWrite(EN_B, 80);
+  digitalWrite(IN_1, 1);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 1);
 }
 
-void runBackward()
-{
-    analogWrite(EN_A, 80);
-    analogWrite(EN_B, 80);
-    digitalWrite(IN_1, 0);
-    digitalWrite(IN_2, 1);
-    digitalWrite(IN_3, 1);
-    digitalWrite(IN_4, 0);
+void runBackward() {
+  analogWrite(EN_A, 80);
+  analogWrite(EN_B, 80);
+  digitalWrite(IN_1, 0);
+  digitalWrite(IN_2, 1);
+  digitalWrite(IN_3, 1);
+  digitalWrite(IN_4, 0);
 }
 
-void runLeft()
-{
-    analogWrite(EN_A, 255);
-    analogWrite(EN_B, 150);
-    digitalWrite(IN_1, 1);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 1);
-    digitalWrite(IN_4, 0);
+void runLeft() {
+  analogWrite(EN_A, 255);
+  analogWrite(EN_B, 150);
+  digitalWrite(IN_1, 1);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 1);
+  digitalWrite(IN_4, 0);
 }
 
-void runRight()
-{
-    analogWrite(EN_A, 150);
-    analogWrite(EN_B, 255);
-    digitalWrite(IN_1, 0);
-    digitalWrite(IN_2, 1);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 1);
+void runRight() {
+  analogWrite(EN_A, 150);
+  analogWrite(EN_B, 255);
+  digitalWrite(IN_1, 0);
+  digitalWrite(IN_2, 1);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 1);
 }
 
-void runBackLeft()
-{
-    analogWrite(EN_A, 255);
-    analogWrite(EN_B, 255);
-    digitalWrite(IN_1, 1);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 1);
+void runBackLeft() {
+  analogWrite(EN_A, 255);
+  analogWrite(EN_B, 255);
+  digitalWrite(IN_1, 1);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 1);
 }
 
-void runBackRight()
-{
-    analogWrite(EN_A, 255);
-    analogWrite(EN_B, 255);
-    digitalWrite(IN_1, 1);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 1);
+void runBackRight() {
+  analogWrite(EN_A, 255);
+  analogWrite(EN_B, 255);
+  digitalWrite(IN_1, 1);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 1);
 }
 
-void round360()
-{
-    analogWrite(EN_A, 150);
-    analogWrite(EN_B, 0);
-    digitalWrite(IN_1, 1);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 1);  
+void round360() {
+  analogWrite(EN_A, 150);
+  analogWrite(EN_B, 0);
+  digitalWrite(IN_1, 1);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 1);
 }
 
-void stopMotor()
-{
-    analogWrite(EN_A, 0);
-    analogWrite(EN_B, 0);
-    digitalWrite(IN_1, 0);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 0);
+void stopMotor() {
+  analogWrite(EN_A, 0);
+  analogWrite(EN_B, 0);
+  digitalWrite(IN_1, 0);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 0);
 }
 
-void attackEnemy()
-{
-    analogWrite(EN_A, 255);
-    analogWrite(EN_B, 255);
-    digitalWrite(IN_1, 1);
-    digitalWrite(IN_2, 0);
-    digitalWrite(IN_3, 0);
-    digitalWrite(IN_4, 1);
+void attackEnemy() {
+  analogWrite(EN_A, 255);
+  analogWrite(EN_B, 255);
+  digitalWrite(IN_1, 1);
+  digitalWrite(IN_2, 0);
+  digitalWrite(IN_3, 0);
+  digitalWrite(IN_4, 1);
 }
 
-void findEnemy()
-{
-    if(Left_IR.distance() <= 10 || Right_IR.distance() <= 10)
-    {
-        attackEnemy();
-        delay(500);
-        stopMotor();
-        delay(100);
-        runBackward();
-        delay(500);
-    }
-    else if (Backward_IR.distance() <= 30)
-    {
-        round360();
-        delay(500);
-        attackEnemy();
-        delay(500);
-        stopMotor();
-        delay(100);
-        runBackward();
-        delay(500);        
-    }
-    else
-    {
-        if(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 1 || digitalRead(LEFT_DETECT_LINE_SENSOR) == 1)
-        {
-            runBackward();
-            delay(500);        
-        }
-        else if(digitalRead(BACK_DETECT_LINE_SENSOR) == 1)
-        {
-            runForward();
-            delay(500);
-        }
-        else
-        {
-            runForward();
-            delay(500);            
-            round360();
-            delay(500);
-            runBackward();
-            delay(500);
-            round360();
-            delay(500); 
-        }
-    }
-}
-
-void avoidToDropOut()
-{
-    if(digitalRead(RIGHT_DETECT_LINE_SENSOR) == 1 || digitalRead(LEFT_DETECT_LINE_SENSOR) == 1)
-    {
-        runBackward();
-        delay(500);
-    }
-        
-    else if(digitalRead(BACK_DETECT_LINE_SENSOR) == 1)
-    {
-        runForward();
-        delay(200);
-    }
+void findEnemy() {
+  if (Left_IR.distance() <= 10 || Right_IR.distance() <= 10) {
+    attackEnemy();
+    delay(500);
     stopMotor();
     delay(100);
+    runBackward();
+    delay(500);
+  } else if (Backward_IR.distance() <= 30) {
+    round360();
+    delay(500);
+    attackEnemy();
+    delay(500);
+    stopMotor();
+    delay(100);
+    runBackward();
+    delay(500);
+  } else {
+    if (digitalRead(RIGHT_DETECT_LINE_SENSOR) == 1 || digitalRead(LEFT_DETECT_LINE_SENSOR) == 1) {
+      runBackward();
+      delay(500);
+    } else if (digitalRead(BACK_DETECT_LINE_SENSOR) == 1) {
+      runForward();
+      delay(500);
+    } else {
+      runForward();
+      delay(500);
+      round360();
+      delay(500);
+      runBackward();
+      delay(500);
+      round360();
+      delay(500);
+    }
+  }
 }
 
-void firstStart()
-{
-    
+void avoidToDropOut() {
+  if (digitalRead(RIGHT_DETECT_LINE_SENSOR) == 1 || digitalRead(LEFT_DETECT_LINE_SENSOR) == 1) {
+    if (millis() - time < 500) {
+      runBackward();
+    } 
+    else
+      {
+          stopMotor();
+          time = millis(); 
+      }
+  }
+
+  else if (digitalRead(BACK_DETECT_LINE_SENSOR) == 1) 
+  {
+    if (millis() - time < 500) {
+      runForward();
+    } 
+    else
+    {
+        stopMotor();
+        time = millis();
+    }
+      
+  }
+
+}
+
+void firstStart() {
 }
